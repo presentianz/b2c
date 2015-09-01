64b045d7bacd96e0f3c9659d7223016d1482c73d
Charis Wan <wnjgon1990@gmail.com>
2015-09-01 14:58:23 +1200

    updated home page and merged index2

--- frontend/js/shop.js
+++ frontend/js/shop.js
@@ -1,19 +1,7 @@
 
-var total; var curt = 0;
 $(document).ready(function() {
 
 	$('#myCarousel').carousel();
-
-	total = $("#index img").length; 
-	$("#index img").each(function(){
-		var image = new Image(); 
-		image.src = $(this).attr('src'); 
-		if (image.complete){ 
-			imageLoaded(); 
-		} else{ 
-			image.onload = imageLoaded;
-		} 
-	});
 
 	$('#gototop').click(function(){
          window.scrollTo(0,0);
@@ -47,14 +35,6 @@ $(document).ready(function() {
 
 });
 
-function imageLoaded(){ 
-	curt++; 
-	$("#indexpro").css("width",curt/total*100+"%");
-	if(curt==total){ 
-		$(".progress").css("display","none");
-		$("#index").css("display","block"); 
-	}
-}
 
 
 
