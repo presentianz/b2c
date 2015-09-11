82336ceabd18d6e1761b27d96dd23f50ef1a99f7
gigantic2 <gigantic222@gmail.com>
2015-09-11 22:41:59 +1200

    solve the fuk category infinite classification

--- src/AppBundle/Controller/Page/DefaultController.php
+++ src/AppBundle/Controller/Page/DefaultController.php
@@ -6,6 +6,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+use AppBundle\Entity\Category;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 class DefaultController extends Controller
 {
     /**
@@ -15,4 +19,48 @@ class DefaultController extends Controller
     {
         return $this->render('Page/default/index.html.twig');
     }
+
+    // /**
+    //  * @Route("/test")
+    //  */
+    // public function testAction()
+    // {
+    //     $em = $this->getDoctrine()->getManager();
+    //     $repo = $em->getRepository('AppBundle:Category');
+    //     // $food = new Category();
+    //     // $food->setName('Food');
+    //     $food = $repo->findOneByName('Food');
+    //     $meat = new Category();
+    //     $meat->setName('Meat');
+    //     $meat->setParent($food);
+
+    //     // $fruits = new Category();
+    //     // $fruits->setName('Fruits');
+    //     // $fruits->setParent($food);
+
+    //     // $vegetables = new Category();
+    //     // $vegetables->setName('Vegetables');
+    //     // $vegetables->setParent($food);
+
+    //     // $carrots = new Category();
+    //     // $carrots->setName('Carrots');
+    //     // $carrots->setParent($vegetables);
+
+    //     //$em->persist($meat);
+    //     // $em->persist($fruits);
+    //     // $em->persist($vegetables);
+    //     // $em->persist($carrots);
+    //     // $meat = $repo->findOneByName('Meat');
+    //     // $repo->removeFromTree($meat);
+    //     // $em->clear();
+    //     // $data = $repo->childrenHierarchy();
+    //     //$carrots = $repo->findOneByName('Carrots');
+    //     $food = $repo->findOneByName('Food');
+    //     $children = $repo->children($food);
+    //     //$path = $repo->getPath($carrots);
+    //     return $this->render('Page/default/test.html.twig', array(
+    //         'data' => $children,
+    //         ));
+
+    // }
 }
--- src/AppBundle/Repository/CategoryRepository.php
+++ src/AppBundle/Repository/CategoryRepository.php
@@ -2,12 +2,12 @@
 
 namespace AppBundle\Repository;
 
-use Doctrine\ORM\EntityRepository;
+use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
 
 /**
  * CatecoryRepository
  */
-class CategoryRepository extends EntityRepository
+class CategoryRepository extends NestedTreeRepository
 {
     public function getCategoryProducts($id, $page, $sort, $item_no)
     {
@@ -41,31 +41,39 @@ class CategoryRepository extends EntityRepository
         $item_no = $item_no <= 20 ? 20 : ($item_no <= 40 ? 40 : 60);
 
         $gEM = $this->getEntityManager();
+        $this_root = $gEM->getRepository('AppBundle:Category')->findOneById($id);
+        $children = $gEM->getRepository('AppBundle:Category')->children($this_root);
+        $ids = [$id];
+        foreach ($children as $child) {
+            array_push($ids, $child->getId());
+        }
 
         //find data
         $query_products = $gEM->createQuery(
                 'SELECT c.name AS category_name, p.id, p.name, p.price AS price, p.price_discounted, p.soldNo AS soldNo, p.inventory, p.status, p.updateAt 
                 FROM AppBundle:Category c JOIN c.products p 
-                WHERE c.id = :id 
+                WHERE c.id IN (:ids) 
                 ORDER BY '.$orderby
             )
             ->setFirstResult(($page-1)*$item_no)
             ->setMaxResults($item_no)
-            ->setParameter('id', $id);
+            ->setParameter('ids', $ids);
         $products = $query_products->getResult();
 
         //find total
         $query_total = $gEM->createQuery(
                 'SELECT COUNT(p.id) AS total 
                 FROM AppBundle:Category c JOIN c.products p 
-                WHERE c.id = :id'
+                WHERE c.id IN (:ids)'
             )
-            ->setParameter('id', $id);
+            ->setParameter('ids', $ids);
         $total = $query_total->getResult();
 
         $data = array();
         $data['products'] = $products;
         $data['total_page'] = ceil($total[0]['total']/$item_no);
+        $data['path'] = $gEM->getRepository('AppBundle:Category')->getPath($this_root);
+        $data['children'] = $gEM->getRepository('AppBundle:Category')->children($this_root, true);
 
         return $data;
     }
