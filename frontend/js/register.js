70afdc61c275e8f89955664c2dc2ac869bf4d602
Charis Wan <wnjgon1990@gmail.com>
2015-09-24 03:25:51 +1200

    update register js bug and address js

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -18,20 +18,16 @@ function bitTotal(num) {
     return modes;
 }
 
-var flag1 = flag2 = flag3 = flag4 = flag5=false;
+var flag1 = flag2 = flag3 = flag4 = flag5 = false;
 
 $(document).ready(function () {
-    $(".controls .error").css("display", "none");
-    $(".controls .check-fa").css("display", "none");
     $("input[name=email]").blur(function (e) {
         var email = $("input[name=email]").val();
         if (email == "") {
             flag1 = false;
-          //  $("input[name=email]").parent().find(".error").find("h6").html("邮箱不能为空");
             wrong("input[name=email]");
         }
         else {
-            console.log("worng email");
             var reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
             var result = reg.test(email);
             if (result == false) {
@@ -49,95 +45,98 @@ $(document).ready(function () {
     });
 
     $("input[name=username]").blur(function (e) {
-         var username = $("input[name=username]").val();
-        if (username == "") {
-            flag2 = false;
-          //  $("input[name=username]").parent().find(".error").find("h6").html("用户名不能为空");
-            wrong("input[name=username]");
-        }
-        else {
-            flag2 = true;
-            correct("input[name=username]");
-        }
+       var username = $("input[name=username]").val();
+       if (username == "") {
+        flag2 = false;
+        wrong("input[name=username]");
+    }
+    else {
+        flag2 = true;
+        correct("input[name=username]");
+    }
         //$("input[name=username]").parent().find(".error").find("h6").html("用户名已经注册过");
     });
 
     $("input[name=psd1]").blur(function (e) { 
-     var psd1 = $("input[name=psd1]").val();  
-        var reg = /^[\w]{6,20}$/;
-        var result = reg.test(psd1);
-        if (psd1 == "") {
-            flag3 = false;
-        //  $("input[name=psd1]").parent().find(".error").find("h6").html("密码不能为空");
-            wrong("input[name=psd1]");
-        }
-        else if (result == false) {
-            flag3 = false;
-           $("input[name=psd1]").parent().find(".error").find("h6").html("密码需要至少6个字节");
-            wrong("input[name=psd1]");
-        }
-        else {
-            flag3 = true;
-            correct("input[name=psd1]");
-            Modes = 0;
-            for (i = 0; i < psd1.length; i++) {
+       var psd1 = $("input[name=psd1]").val();  
+       var reg = /^[\w]{6,20}$/;
+       var result = reg.test(psd1);
+       if (psd1 == "") {
+        flag3 = false;
+        wrong("input[name=psd1]");
+        $(".security-level").find("span").removeClass("orange red green");
+    }
+    else if (result == false) {
+        flag3 = false;
+        $("input[name=psd1]").parent().find(".error").find("h6").html("密码需要至少6个字节");
+        wrong("input[name=psd1]");
+    }
+    else {
+        flag3 = true;
+        correct("input[name=psd1]");
+        Modes = 0;
+        for (i = 0; i < psd1.length; i++) {
                 //测试每一个字符的类别并统计一共有多少种模式.    
                 Modes |= CharMode(psd1.charCodeAt(i));
             }
             switch (bitTotal(Modes)) {
                 case 1:
-                    $(".security-level").find("span").removeClass("active");
-                    if (psd1.length >= 15)
-                        $(".block2").addClass("active");
-                    else
-                        $(".block1").addClass("active");
-                    break;
+                $(".security-level").find("span").removeClass("orange red green");
+                if (psd1.length >= 15) {
+                    $(".block2").addClass("orange");
+                    $(".block1").addClass("orange");
+                }
+                else {
+                    $(".block1").addClass("red");
+                }
+                break;
                 case 2:
-                    $(".security-level").find("span").removeClass("active");
-                    $(".block2").addClass("active");
-                    break;
+                $(".security-level").find("span").removeClass("orange red green");
+                $(".block2").addClass("orange");
+                $(".block1").addClass("orange");
+                break;
                 default:
-                    $(".security-level").find("span").removeClass("active");
-                    $(".block3").addClass("active");
-                    break;
+                $(".security-level").find("span").removeClass("orange red green");
+                $(".block3").addClass("green");
+                $(".block2").addClass("green");
+                $(".block1").addClass("green");
+                break;
             }
         }
 
     });
 
-    $("input[name=psd2]").blur(function (e) {
-         var psd1 = $("input[name=psd1]").val();
-        var psd2 = $("input[name=psd2]").val();
+$("input[name=psd2]").blur(function (e) {
+   var psd1 = $("input[name=psd1]").val();
+   var psd2 = $("input[name=psd2]").val();
+   if (psd2 == "") {
+    flag4 = false;
+    wrong("input[name=psd2]");
+}
+else {
+    if (psd1 != psd2) {
+        flag4 = false;
+        wrong("input[name=psd2]");
+        $("input[name=psd2]").parent().find(".error").find("h6").html("密码不匹配");
+    }
+    else {
+        flag4 = true;
+        correct("input[name=psd2]");
+    }
+}
+});
 
-        if (psd2 == "") {
-            flag4 = false;
-           // $("input[name=psd2]").parent().find(".error").find("h6").html("确认密码不能为空");
-            wrong("input[name=psd2]");
-        }
-        else {
-            if (psd1 != psd2) {
-                flag4 = false;
-                wrong("input[name=psd2]");
-                $("input[name=psd2]").parent().find(".error").find("h6").html("密码不匹配");
-            }
-            else {
-                flag4 = true;
-                correct("input[name=psd2]");
-            }
-        }
-    });
-
-    $("input[id=checkinfo]").blur(function (e) {
-        var checkinfo = document.getElementById("checkinfo");
-        if(checkinfo.checked){
-             flag5=true;
-             correct("input[id=checkinfo]");
-        }
-        else {
-            flag5=false;
-            wrong("input[id=checkinfo]");
-        }
-    });
+$("input[id=checkinfo]").blur(function (e) {
+    var checkinfo = document.getElementById("checkinfo");
+    if(checkinfo.checked){
+       flag5=true;
+       correct("input[id=checkinfo]");
+   }
+   else {
+    flag5=false;
+    wrong("input[id=checkinfo]");
+}
+});
 
 
 });
@@ -145,16 +144,16 @@ $(document).ready(function () {
 
 
 function correct(checking) {
-    $(checking).parent().find(".check-fa").addClass("fa-check-circle");
-    $(checking).parent().find(".check-fa").removeClass("fa-times-circle");
-    $(checking).parent().find(".check-fa").css("display", "inline-block");
+    $(checking).parent().find(".check-icon").addClass("fa-check-circle");
+    $(checking).parent().find(".check-icon").removeClass("fa-times-circle");
+    $(checking).parent().find(".check-icon").css("display", "inline-block");
     $(checking).parent().find(".error").css("display", "none");
 
 }
 function wrong(checking) {
-    $(checking).parent().find(".check-fa").removeClass("fa-check-circle");
-    $(checking).parent().find(".check-fa").addClass("fa-times-circle");
-    $(checking).parent().find(".check-fa").css("display", "inline-block");
+    $(checking).parent().find(".check-icon").removeClass("fa-check-circle");
+    $(checking).parent().find(".check-icon").addClass("fa-times-circle");
+    $(checking).parent().find(".check-icon").css("display", "inline-block");
     $(checking).parent().find(".error").css("display", "inline-block");
 
 }
@@ -162,50 +161,71 @@ function wrong(checking) {
 
 function reigster() {
     if (flag1 && flag2 && flag3 && flag4 && flag5) {
-          console.log("success to regiseter");
+         console.log("success");
+         console.log("success to register");
+         var email = $("input[name=email]").val();
+        var username = $("input[name=username]").val();
+        var psd1 = $("input[name=psd1]").val();
+        console.log(email + username + psd1);
+        var user = {"email" : email, "name" : username, "password": psd1};
+        var userObject = [];
+        userObject.push(user);
+        console.log(userObject);
         return true;
+
     }     
     else if($("input[name=email]").val()==""){
-        console.log("fail to regiseter");
         wrong("input[name=email]");
         return false;
     }
-      else if($("input[name=username]").val()==""){
-        console.log("fail to regiseter");
-         wrong("input[name=username]");
+    else if($("input[name=username]").val()==""){
+       wrong("input[name=username]");
+       return false;
+   }
+   else if($("input[name=psd1]").val()==""){
+       wrong("input[name=psd1]");
+       return false;
+   }
+   else if($("input[name=psd2]").val()==""){
+       wrong("input[name=psd2]");
+       return false;
+   }
+   else if(!checkinfo.checked){
+       wrong("input[id=checkinfo]");
+       return false;
+   }
+   else {
+   
         return false;
-    }
-      else if($("input[name=psd1]").val()==""){
-        console.log("fail to regiseter");
-         wrong("input[name=psd1]");
-        return false;
-    }
-      else if($("input[name=psd2]").val()==""){
-        console.log("fail to regiseter");
-         wrong("input[name=psd2]");
-        return false;
-    }
+}
 
-    else {
-        console.log("fail to regiseter");
-        return false;
-    }
 
-        
 }
 
 function login() {
     if (flag1 && flag3){
-        console.log("success to login");
-        location.href="index.html";
+       // location.href="index.html";
+        console.log("success");
+         console.log("success to register");
+         var email = $("input[name=email]").val();
+        var psd1 = $("input[name=psd1]").val();
+        console.log(email + psd1);
+
         return true;
-    }
-         
-    else{
-        console.log("fail to login");
+    } 
+    else if($("input[name=email]").val()==""){
+        wrong("input[name=email]");
         return false;
     }
-         
+    else if($("input[name=psd1]").val()==""){
+       wrong("input[name=psd1]");
+       return false;
+   }      
+   else{
+    console.log("fail to login");
+    return false;
+}
+
 }
 
 
--- frontend/js/shop.js
+++ frontend/js/shop.js
@@ -18,7 +18,26 @@ $(document).ready(function () {
     });
 
 
+
+
+
 });
+
+
+function correct(checking) {
+    $(checking).parent().find(".check-icon").addClass("fa-check-circle");
+    $(checking).parent().find(".check-icon").removeClass("fa-times-circle");
+    $(checking).parent().find(".check-icon").css("display", "inline-block");
+    $(checking).parent().find(".error").css("display", "none");
+
+}
+function wrong(checking) {
+    $(checking).parent().find(".check-icon").removeClass("fa-check-circle");
+    $(checking).parent().find(".check-icon").addClass("fa-times-circle");
+    $(checking).parent().find(".check-icon").css("display", "inline-block");
+    $(checking).parent().find(".error").css("display", "inline-block");
+
+}
 
 
 function popup(i) {
@@ -30,17 +49,72 @@ function popup(i) {
     $("#"+e).css("display", "block");
     $("body").css("overflow","hidden");   
     $("#"+e).load(e+".html", function() {
-     $('.save').click(function () {
-        $("#"+e).css("display", "none");
-         $("body").css("overflow","auto");  
-    });
 
-     $('#'+e +' .content > a').on("click", function (e) {
-        var currentAttrValue = jQuery(this).attr('href');
-        $(currentAttrValue + '.part').addClass('active').siblings().removeClass('active');
-        e.preventDefault();
-    });
- });
+             //new or edit address
+
+             $("input[name=province]").blur(function (e) {
+                console.log("provice");
+                var e = $("input[name=province]").val();
+                if (e == "") {
+                    wrong("input[name=province]");
+                }
+                else {
+                    correct("input[name=province]");
+                }
+            });
+
+             $("input[name=city]").blur(function (e) {
+                 var e = $("input[name=city]").val();
+                 if (e == "") {
+                    wrong("input[name=city]");
+                }
+                else {
+                    correct("input[name=city]");
+                }
+            });
+
+             $("input[name=name]").blur(function (e) {
+                 var e = $("input[name=name]").val();
+                 if (e == "") {
+                    wrong("input[name=name]");
+                }
+                else {
+                    correct("input[name=name]");
+                }
+            });
+
+             $("input[name=phone]").blur(function (e) {
+                 var e = $("input[name=phone]").val();
+                 if (e == "") {
+                    wrong("input[name=phone]");
+                }
+                else {
+                    correct("input[name=phone]");
+                }
+            });
+
+             $("textarea[name=address]").blur(function (e) {
+                 var e = $("textarea[name=address]").val();
+                 if (e == "") {
+                    wrong("textarea[name=address]");
+                }
+                else {
+                    correct("textarea[name=address]");
+                }
+            });
+
+             $('.save').click(function () {
+                $("#"+e).css("display", "none");
+                $("body").css("overflow","auto");  
+                });
+
+                $('#'+e +' .content > a').on("click", function (e) {
+                    var currentAttrValue = jQuery(this).attr('href');
+                    $(currentAttrValue + '.part').addClass('active').siblings().removeClass('active');
+                    e.preventDefault();
+
+            });
+         });
 
 }
 
