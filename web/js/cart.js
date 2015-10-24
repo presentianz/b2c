3d8d17387ebcf2df8b5ce97201a2f45675a49111
Charis Wan <wnjgon1990@gmail.com>
2015-10-24 18:17:11 +1300

    updated select all

--- web/js/cart.js
+++ web/js/cart.js
@@ -44,3 +44,10 @@
         })
     })
 })();
+
+function selectAll(source) {
+  var checkboxes = document.getElementsByName('product-id[]');
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
