472d5632949fbbbb3b0015f87b852e60f25110f6
Charis Wan <wnjgon1990@gmail.com>
2015-11-22 17:32:58 +1300

    update admin pagination

--- web/js/pagination.js
+++ web/js/pagination.js
@@ -7,9 +7,19 @@ var $pagination = $('.select-page-number');
     var idx = $lis.index($lis.filter('.paginate_active')) || 0;
     var total = $lis.filter('.paginate_button').length;
     var $toHighlight;
-    if(idx - 2 < 0 ) $toHighlight = $lis.slice(idx, idx + 5);
-    else if(idx + 3 > total) $toHighlight = $lis.slice(idx - 4, idx);
-    else $toHighlight = $lis.slice(idx - 2, idx + 3);
+    if(idx <= 0 ) {
+        $toHighlight = $lis.slice(idx, idx + 5);
+        $pagination.find('a.first').filter('.paginate_button').hide();
+        $pagination.find('a.previous').filter('.paginate_button').hide();        
+    } 
+    else if(idx == 1 ) $toHighlight = $lis.slice(idx - 1, idx + 4);
+    else if(idx + 2 == total) $toHighlight = $lis.slice(idx -3, idx + 2 );
+    else if(idx + 1 >= total) {
+        $toHighlight = $lis.slice(idx - 4, idx + 3 );
+        $pagination.find('a.next').filter('.paginate_button').hide();
+        $pagination.find('a.last').filter('.paginate_button').hide();   
+    } 
+    else $toHighlight = $lis.slice(idx - 2, idx + 3);  
     $toHighlight.addClass('showed')
     $lis.filter(".paginate_active").addClass('showed');
     $lis.filter('.showed').show();
