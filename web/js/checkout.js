0aa9ddce5fbeb34a9fb8c7291a462f7e1c85316c
Charis Wan <wnjgon1990@gmail.com>
2016-02-06 23:27:54 +1300

    save to merge

--- web/js/checkout.js
+++ web/js/checkout.js
@@ -1,40 +1,4 @@
- (function () {
-        $('.btn-pay').click(function() {
-        $('.bg-mask').css('display','block');
-        $('.pop-cart').css('display','block');
-        });
 
-
-        $('.btn-confirm').click(function () {
-            $this = $(this);
-        var productId = $this.attr("data-id");
-        $.ajax({
-            url: "{{ path('order_generation') }}",
-            method: "GET",
-            data: {
-                id : productId,
-            },
-            dataType: "json"
-        })
-        .done(function (rep) {
-            console.log(rep);
-            if (rep.granted) {
-                alert("成功！");
-                window.location.replace("{{ path('order_confirm') }}"+'?id='+rep.id);
-                
-            }
-            else {
-                alert("。。");
-            }
-        })
-        });
-
-
-         $("input[name='region']").blur(function (e) {
-            alert("hehe");
-
-         });
-    })();
 
 
 
