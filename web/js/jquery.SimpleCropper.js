35e33e3df2efa6a7af8f269e3aa862598e8b44af
genghaokun <haokun.geng@gmail.com>
2016-04-30 12:10:15 +1200

    user_profile page updated. Errors are fixed.

--- web/js/jquery.SimpleCropper.js
+++ web/js/jquery.SimpleCropper.js
@@ -24,8 +24,8 @@
         var ias = null;
         var original_data = null;
         var jcrop_api;
-        var bottom_html = "<input type='file' id='fileInput' name='files[]'/ accept='image/*'><canvas id='myCanvas' style='display:none;'></canvas><div id='modal'></div><div id='preview'><div class='buttons'><div class='cancel'></div><div class='ok'></div></div></div>";
-        $('body').append(bottom_html);
+        //var bottom_html = "<input type='file' id='fileInput' name='files[]'/ accept='image/*'><canvas id='myCanvas' style='display:none;'></canvas><div id='modal'></div><div id='preview'><div class='buttons'><div class='cancel'></div><div class='ok'></div></div></div>";
+        //$('body').append(bottom_html);
 
         //add click to element
         this.click(function () {
